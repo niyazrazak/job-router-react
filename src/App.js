d27a12e32c56d85bea8c3d74091e700f111918e4
Niyaz Razak <niyasibnurazak@gmail.com>
2023-08-24 15:31:40 +0300

    fix: deploy

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Contact from "./pages/help/Contact";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout/>}>
+    <Route exact path="/" element={<RootLayout/>}>
       <Route index element={<Home />}></Route>
       <Route path="about" element={<About />}></Route>
       <Route path="help" element={<HelpLayout/>}>
